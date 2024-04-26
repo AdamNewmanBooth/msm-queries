e43194a54346267062c87d7626c3e2bbd6430234
AdamNewmanBooth <164922100+AdamNewmanBooth@users.noreply.github.com>
2024-04-26 21:52:19 +0000

    Commit3

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ get("/directors", {:controller => "directors", :action => "index"})
 
 get("/directors/:the_id", {:controller => "directors", :action => "show"})
 
+get("/movies", {:controller => "movies", :action => "index"})
+
+get("/movies/:the_id", {:controller => "movies", :action => "show"})
+
 end
